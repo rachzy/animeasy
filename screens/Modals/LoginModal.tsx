c911f49456ae734350636c987708ac47d452efc0
Henrique <henriqueferreira.fox@gmail.com>
2022-10-03 18:50:41 -0300

    Adding user interaction and error handling

--- screens/Modals/LoginModal.tsx
+++ screens/Modals/LoginModal.tsx
@@ -1,10 +1,18 @@
-import { Text, TextInput, View, StyleSheet, Pressable } from "react-native";
+import {
+  Text,
+  TextInput,
+  View,
+  StyleSheet,
+  Pressable,
+  TextInputChangeEventData,
+} from "react-native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 
 import { RootStackParamList } from "../../types";
 
 import DefaultButton from "../../components/DefaultButton";
 import { useState } from "react";
+import { TextInputProps } from "react-native";
 
 const LoginModal: React.FC<
   NativeStackScreenProps<RootStackParamList, "LoginModal">
@@ -18,28 +26,72 @@ const LoginModal: React.FC<
     },
   });
 
+  const [inputValues, setInputValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [errorValues, setErrorValues] = useState({
+    email: "",
+    password: "",
+  });
+
   const [toggleButtonLoading, setToggleButtonLoading] = useState(false);
 
   const handleButtonPress = () => {
     setToggleButtonLoading(true);
+    setErrorValues({
+      email: "",
+      password: "",
+    });
+    setTimeout(() => {
+      setToggleButtonLoading(false);
+      setErrorValues({
+        email: "Incorrect email or username",
+        password: "Wrong password",
+      });
+    }, 2000);
+  };
+
+  const handleInputChange = (name: "email" | "password", text: string) => {
+    setInputValues((currentValue) => {
+      return {
+        ...currentValue,
+        [name]: text,
+      };
+    });
   };
 
   return (
     <View style={styles.mainContainer}>
       <Text style={styles.title}>Welcome back!</Text>
       <TextInput
-        style={styles.input}
+        style={[styles.input, errorValues.email ? styles.inputError : {}]}
         textContentType={"emailAddress" || "text"}
         placeholder="Email or username"
         placeholderTextColor="gray"
+        value={inputValues.email}
+        onChangeText={(text) => {
+          handleInputChange("email", text);
+        }}
       />
+      {errorValues.email && (
+        <Text style={styles.errorMessage}>{errorValues.email}</Text>
+      )}
       <TextInput
-        style={styles.input}
+        style={[styles.input, errorValues.password ? styles.inputError : {}]}
         secureTextEntry={true}
         textContentType={"password"}
         placeholder="Password"
         placeholderTextColor="gray"
+        value={inputValues.password}
+        onChangeText={(text) => {
+          handleInputChange("password", text);
+        }}
       />
+      {errorValues.password && (
+        <Text style={styles.errorMessage}>{errorValues.password}</Text>
+      )}
       <DefaultButton
         onPress={handleButtonPress}
         loadingAnimation={toggleButtonLoading}
@@ -82,6 +134,16 @@ const styles = StyleSheet.create({
     shadowRadius: 20.0,
     elevation: 24,
     opacity: 1,
+  },
+  inputError: {
+    borderWidth: 1,
+    borderColor: "red",
+  },
+  errorMessage: {
+    color: "red",
+    marginLeft: 5,
+    marginBottom: 20,
+    marginTop: -10,
   },
   forgotPasswordLabel: {
     color: "#03DAC5",
